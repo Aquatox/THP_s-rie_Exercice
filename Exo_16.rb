29c78d3afed2e0067c29d90066af429b96e0e8a7
Aquatox <62962573+Aquatox@users.noreply.github.com>
2020-04-08 00:02:56 +0200

    Add files via upload

---
+++ Exo_16.rb
@@ -0,0 +1,18 @@
+puts "Bonjour, quel est ton âge ?"
+age = Integer gets.chomp
+c=1
+age2= age-  c
+while c < age
+ 
+    print "il y a "
+    print c
+    print " ans"
+    puts " "
+    print " Tu avais "
+    print age2
+    print " ans "
+    puts " "
+    age2=age2-1
+    c=c+1
+    
+end
